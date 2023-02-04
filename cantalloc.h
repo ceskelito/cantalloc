c7751e9592b8a111afca8c1509af1a8a57b1f7c4
Claudio Cantale <ccantale@e3r5p8.42roma.it>
2023-02-04 14:04:25 +0100

    cantalloc
    Added comments to the .h file

--- cantalloc.h
+++ cantalloc.h
@@ -13,9 +13,17 @@ typedef struct s_alloc
 	struct s_alloc	*next;
 }	t_alloc;
 
-/* funcs */
+/* Allocates size bytes of memory with malloc() and saves
+** the pointer in a static linked list, before returning it.
+** Returns NULL if malloc fails. */
 void	*cantalloc(size_t size);
+
+/* Allocates size * count bytes of memory with malloc(), fills
+** everything with 0s and saves the pointer in a static linked list,
+** before returning it. Returns NULL if malloc fails.*/
 void	*ccantalloc(size_t size, size_t count);
+
+/* Frees all the memory previously allocated by cantalloc and ccantalloc. */
 void	cantalloc_clean(void);
 
 #endif
